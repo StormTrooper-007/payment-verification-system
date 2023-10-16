ebfa7788975513e3202aa5fce9db1e8937df3641
collins <chigbo.c.o@gmail.com>
2023-10-16 22:51:47 +0200

    added docker-compose.yml to orchestrate the payment-system-verification frontend, main app and websocket app

--- src/main/java/com/example/paymentverificationsystem/PaymentVerificationSystemApplication.java
+++ src/main/java/com/example/paymentverificationsystem/PaymentVerificationSystemApplication.java
@@ -1,8 +1,9 @@
 package com.example.paymentverificationsystem;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 
 @SpringBootApplication
--- src/main/java/com/example/paymentverificationsystem/models/OrderDetails.java
+++ src/main/java/com/example/paymentverificationsystem/models/OrderDetails.java
@@ -3,10 +3,12 @@ package com.example.paymentverificationsystem.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document("orderDetails")
 public class OrderDetails {
     private String id;
     private String name;
